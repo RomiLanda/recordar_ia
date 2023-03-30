0888e4584a62c9da2d71e9305de403acf99bfc10
RomiLanda <romailanda@gmail.com>
2023-03-30 16:50:16 -0300

    estructura y readme

---
+++ src/src.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def procesar_imgs(path_in, path_out): #TODO agregar corrector jurídico si es necesario
+    data = [] # 
+    noticia_procesada = {"Diario": [],
+                                 "Fecha": [],
+                                 "Volanta": [],
+                                 "Título": [],
+                                 "Cuerpo": [],
+                                 "Copete": [],
+                                 "Destacado": [],
+                                 "Epígrafe": []}
+    
+    reporte = pd.DataFrame(data=data)
+    reporte.to_csv(path_out + 'reporte.csv', index=False)
+    print(f'Procesamiento finalizado. Los resultados fueron guardados en el directorio: {path_out}.')
