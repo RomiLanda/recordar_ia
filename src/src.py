fe399c280b922cf4bbdf6121766acfb09d7016f5
RomiLanda <romailanda@gmail.com>
2023-05-19 12:52:56 -0300

    add flag train

--- src/src.py
+++ src/src.py
@@ -2,8 +2,8 @@ import pandas as pd
 from .load_data import create_data_block
 from .neural_network import process
 
-def procesar_imgs(path_in, path_out): #TODO agregar corrector jurídico si es necesario
-    data = create_data_block(path_in, path_out, debug = False)
+def procesar_imgs(path_in, path_out, train_flow = False): #TODO agregar corrector jurídico si es necesario
+    data = create_data_block(path_in, path_out, train_flow, debug = True)
     processed_data = process(data, use_existing_model=True)
     noticia_procesada = {"Diario": [],
                                  "Fecha": [],
