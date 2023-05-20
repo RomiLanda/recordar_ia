7f86911bc86bfc2e5696dc871cb452432916213c
RomiLanda <romailanda@gmail.com>
2023-05-19 21:52:59 -0300

    add TRAIN_FLOW external flag

--- src/src.py
+++ src/src.py
@@ -2,8 +2,11 @@ import pandas as pd
 from .load_data import create_data_block
 from .neural_network import process
 
-def procesar_imgs(path_in, path_out, train_flow = True): #TODO agregar corrector jurídico si es necesario
-    data = create_data_block(path_in, path_out, train_flow, debug = True)
+TRAIN_FLOW = True
+DEBUG = True
+
+def procesar_imgs(path_in, path_out, train_flow = TRAIN_FLOW): #TODO agregar corrector jurídico si es necesario
+    data = create_data_block(path_in, path_out, train_flow, debug = DEBUG)
     processed_data = process(data, train_flow, use_existing_model=False)
     noticia_procesada = {"Diario": [],
                                  "Fecha": [],
